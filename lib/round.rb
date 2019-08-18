6f22a455f9bf558c26572f311662098283b543de
Zac Isaacson <isaacson.zm@gmail.com>
2019-08-17 20:37:48 -0600

    Add take turn method and functionality

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,42 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+  def take_turn(guess)
+     new_turn = Turn.new(guess, current_card)
+     @deck.cards.rotate!
+     @turns << new_turn
+     new_turn
+  end
+
+  def number_correct
+    @turns.select do |turn|
+      if turn.correct? == true
+
+
+      end
+
+  end
+
+  end
+
+
+
+  def number_correct_by_category
+  end
+
+  def percent_correct
+  end
+
+
+
+
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -19,8 +19,43 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, @round
   end
 
-  # def test_round_has_deck
-  #   assert_equal @deck, @round.deck
-  # end
+  def test_round_has_deck
+    assert_equal @deck, @round.deck
+  end
 
-end
+  def test_on_initialization_current_card_is_first_card
+    expected = @card_1
+    result = @round.current_card
+    assert_equal expected, result
+  end
+
+  def test_take_turn_returns_turn
+    guess = "whatever"
+    result = @round.take_turn(guess)
+    assert_instance_of Turn, result
+  end
+
+  def test_guess_matches_turn
+    guess = "Whateverg"
+    result = @round.take_turn(guess)
+    assert_equal guess, result.guess
+  end
+
+  def test_card_returns_a_turn_using_current_card
+    guess = "Whatever"
+    result = @round.take_turn(guess)
+    assert_equal @card_1, result.card
+  end
+
+  def test_how_many_correct
+    assert_equal 1, @turn.number_correct
+  end
+
+
+  end
+
+
+  # def test_next_card_becomes_current_card
+  #   assert_equal @card_2
+  #
+  # end
